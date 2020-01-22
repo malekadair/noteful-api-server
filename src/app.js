b58d2c52477e355f93920d4404554c8c1d0e0885
Grace <mgracebodur@gmail.com>
2020-01-21 19:47:19 -0500

    Initial clean up

--- src/app.js
+++ src/app.js
@@ -13,10 +13,6 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
-
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
