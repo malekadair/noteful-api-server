be4a597fcf1f224890150f1ae7968007b13310a1
Malek Haj-Hussein <malekadair@gmail.com>
2020-01-23 17:53:06 -0600

    changed directory name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://grace_bodur@localhost/noteful-api',
-    TEST_DB_URL: process.env.DB_URL || 'postgresql://grace_bodur@localhost/noteful-api',
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL: process.env.DB_URL || "postgresql://Malek@localhost/noteful-api",
+  TEST_DB_URL: process.env.DB_URL || "postgresql://Malek@localhost/noteful-api"
+};
